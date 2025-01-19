3bb4a11e88c91ab1c878bf5336eec667c40ca5a4
dhruvds12 <dhruv.devgan@yahoo.com>
2025-01-19 17:19:49 +0000

    added timeout + counter

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 // TODO: uncomment the following only on one
 // of the nodes to initiate the pings
-// #define INITIATING_NODE
+#define INITIATING_NODE
 
 // SX1262 has the following connections:
 // NSS pin:   8
@@ -17,7 +17,6 @@ SX1262 radio = new Module(8, 14, 12, 13);
 // #define RADIO_BOARD_AUTO
 // #include <RadioBoards.h>
 // Radio radio = new RadioModule();
-
 
 // save transmission states between loops
 int transmissionState = RADIOLIB_ERR_NONE;
@@ -33,14 +32,18 @@ volatile bool operationDone = false;
 // IMPORTANT: this function MUST be 'void' type
 //            and MUST NOT have any arguments!
 #if defined(ESP8266) || defined(ESP32)
-  ICACHE_RAM_ATTR
+ICACHE_RAM_ATTR
 #endif
-void setFlag(void) {
+void setFlag(void)
+{
   // we sent or received a packet, set the flag
   operationDone = true;
 }
 
-SSD1306Wire  oled_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+SSD1306Wire oled_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+int count = 0;
+unsigned long lastTransmissionTime = 0;          // Track the last transmission time
+const unsigned long transmissionInterval = 1000; // Set timeout (e.g., 1000 ms = 1 seconds)
 
 // Pin mode Controls
 
@@ -48,115 +51,132 @@ SSD1306Wire  oled_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST
 /// @param  void
 void VextON(void)
 {
-  pinMode(Vext,OUTPUT);
+  pinMode(Vext, OUTPUT);
   digitalWrite(Vext, LOW);
   Serial.println("VextON");
-  
 }
 
 /// @brief  Turn Vext OFF
 /// @param   void
-void VextOFF(void) //Vext default OFF
+void VextOFF(void) // Vext default OFF
 {
-  pinMode(Vext,OUTPUT);
+  pinMode(Vext, OUTPUT);
   digitalWrite(Vext, HIGH);
 }
 
-
-void displayString(int x, int y, String str) {
+void displayString(int x, int y, String str)
+{
   oled_display.drawString(x, y, str);
   oled_display.display();
 }
 
-void displayNewString(int x, int y, String str) {
+void displayNewString(int x, int y, String str)
+{
   oled_display.clear();
-	oled_display.display();
+  oled_display.display();
   oled_display.drawString(x, y, str);
   oled_display.display();
 }
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   VextON();
-	delay(100);
+  delay(100);
   oled_display.init();
-	oled_display.clear();
-	oled_display.display();
+  oled_display.clear();
+  oled_display.display();
   delay(100);
 
   // initialize SX1262 with default settings
   Serial.print(F("[SX1262] Initializing ... "));
   displayNewString(0, 0, "[SX1262] Initializing ... ");
   int state = radio.begin(868.0F, 125.0F, 9u, 7u, 18u, 10, 8u);
-  if (state == RADIOLIB_ERR_NONE) {
+  if (state == RADIOLIB_ERR_NONE)
+  {
     Serial.println(F("success!"));
     displayNewString(0, 0, "success!");
-  } else {
+  }
+  else
+  {
     Serial.print(F("failed, code "));
     Serial.println(state);
     std::string error = "failed, code " + std::to_string(state);
     displayNewString(0, 0, error.c_str());
-    while (true) { delay(10); }
+    while (true)
+    {
+      delay(10);
+    }
   }
 
   // set the function that will be called
   // when new packet is received
   radio.setDio1Action(setFlag);
 
-  #if defined(INITIATING_NODE)
-    // send the first packet on this node
-    Serial.print(F("[SX1262] Sending first packet ... "));
-    transmissionState = radio.startTransmit("Hello World!");
-    transmitFlag = true;
-    displayString(0, 0, "Sending first packet ... ");
-  #else
-    // start listening for LoRa packets on this node
-    Serial.print(F("[SX1262] Starting to listen ... "));
-    
-    state = radio.startReceive();
-    if (state == RADIOLIB_ERR_NONE) {
-      Serial.println(F("success!"));
-      displayNewString(0, 0, "Starting to listen ... ");
-    } else {
-      Serial.print(F("failed, code "));
-      Serial.println(state);
-      std::string error = "failed, code " + std::to_string(state);
-      displayNewString(0, 0, error.c_str());
-      while (true) { delay(10); }
+#if defined(INITIATING_NODE)
+  // send the first packet on this node
+  Serial.print(F("[SX1262] Sending first packet ... "));
+  transmissionState = radio.startTransmit("Hello World!");
+  transmitFlag = true;
+  displayString(0, 0, "Sending first packet ... ");
+#else
+  // start listening for LoRa packets on this node
+  Serial.print(F("[SX1262] Listening ... "));
+
+  state = radio.startReceive();
+  if (state == RADIOLIB_ERR_NONE)
+  {
+    Serial.println(F("success!"));
+    displayNewString(0, 0, "Listening ... ");
+  }
+  else
+  {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    std::string error = "failed, code " + std::to_string(state);
+    displayNewString(0, 0, error.c_str());
+    while (true)
+    {
+      delay(10);
     }
-  #endif
+  }
+#endif
 }
 
-void loop() {
+void loop()
+{
   // factory_display.drawString(0, 0, "Hello, world!");
   // check if the previous operation finished
-  if(operationDone) {
+  if (operationDone)
+  {
     // reset flag
     operationDone = false;
 
-    if(transmitFlag) {
+    if (transmitFlag)
+    {
       // the previous operation was transmission, listen for response
       // print the result
-      if (transmissionState == RADIOLIB_ERR_NONE) {
+      if (transmissionState == RADIOLIB_ERR_NONE)
+      {
         // packet was successfully sent
         Serial.println(F("transmission finished!"));
         displayString(0, 40, "transmission finished!");
-
-      } else {
+      }
+      else
+      {
         Serial.print(F("failed, code "));
         Serial.println(transmissionState);
         std::string error = "failed, code " + std::to_string(transmissionState);
         displayString(0, 0, error.c_str());
-
       }
 
       // listen for response
       radio.startReceive();
       transmitFlag = false;
-
-    } else {
+    }
+    else
+    {
       // the previous operation was reception
       // print data and send another packet
       // displayString(0, 0, "Waiting for packet ... ");
@@ -164,10 +184,13 @@ void loop() {
       int state = radio.readData(str);
       oled_display.clear();
 
-      if (state == RADIOLIB_ERR_NONE) {
+      if (state == RADIOLIB_ERR_NONE)
+      {
+        count++;
         // packet was successfully received
         Serial.println(F("[SX1262] Received packet!"));
-        oled_display.drawString(0, 0, "Received packet!");
+        std::string received = "Received packet no: " + std::to_string(count);
+        oled_display.drawString(0, 0, received.c_str());
 
         // print data of the packet
         Serial.print(F("[SX1262] Data:\t\t"));
@@ -188,19 +211,21 @@ void loop() {
         Serial.println(F(" dB"));
         std::string snr = "SNR: " + std::to_string(radio.getSNR()) + " dB";
         oled_display.drawString(0, 30, snr.c_str());
-
       }
       oled_display.display();
 
       // wait a second before transmitting again
       delay(1000);
 
-      // send another one
-      Serial.print(F("[SX1262] Sending another packet ... "));
-      // displayString(0, 0, "Sending another packet ... ");
-      transmissionState = radio.startTransmit("Hello World!");
-      transmitFlag = true;
+      if (millis() - lastTransmissionTime >= transmissionInterval)
+      {
+        Serial.print(F("[SX1262] Sending another packet ... "));
+        transmissionState = radio.startTransmit("Hello World!");
+        transmitFlag = true;
+
+        // Update the last transmission time
+        lastTransmissionTime = millis();
+      }
     }
-  
   }
 }
