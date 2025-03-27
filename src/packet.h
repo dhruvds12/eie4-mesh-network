1e0e154ec085b8475a5c97db61683ce4e99d9686
dhruvds12 <dhruv.devgan@yahoo.com>
2025-03-27 10:35:48 +0000

    packet format + serialisationa & deserialisation

---
+++ src/packet.h
@@ -0,0 +1,148 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <stdint.h>
+#include <Arduino.h>
+
+// Base Header (16 bytes)
+struct BaseHeader {
+  uint32_t destNodeID;   // 4 bytes
+  uint32_t srcNodeID;    // 4 bytes
+  uint32_t packetID;     // 4 bytes
+  uint8_t packetType;    // 1 byte: 0x01=broadcast, 0x02=RREQ, 0x03=RREP, 0x04 = RERR, 0x05 = ACK, 0x06 = data
+  uint8_t flags;         // 1 byte: bitmask for options (e.g., WantAck)
+  uint8_t hopCount;      // 1 byte: TTL/hop count
+  uint8_t reserved;      // 1 byte: reserved
+};
+
+// Extended header for RREQ (6 bytes)
+struct RREQHeader {
+  uint32_t originNodeID;   // 4 bytes: origin of the RREQ
+  uint8_t currentHops;     // 1 byte:  Current number of hops 
+  uint8_t rreqReserved;    // 1 byte:  reserved 
+
+};
+
+// Extended header for RREP (7 bytes)
+struct RREPHeader {
+  uint32_t originNodeID;   // 4 bytes: destination for which route is provided
+  uint16_t lifetime;       // 2 bytes: route lifetime
+  uint8_t numHops;         // 1 byte:  Number of hops using this route
+};
+
+// Extended header for RERR (16 bytes)
+struct RERRHeader {
+    uint32_t originNodeID;       // 4 bytes: Node that reported the issue
+    uint32_t brokenNodeID;       // 4 bytes: Broken nodeID
+    uint32_t originalDestNodeID; // 4 bytes: Original destination
+    uint32_t originalPacketId;   // 4 bytes: Original packetID that will have been overwritten
+};
+
+// Extended header for ACk (4 bytes)
+struct ACKHeader {
+    uint32_t originalPacket; // 4 bytes: If ACK required return the original message id as an ack
+};
+
+// Serialisation and deserialisation functions:
+inline size_t serialiseBaseHeader(const BaseHeader &header, uint8_t* buffer){
+    size_t offset = 0;
+    memcpy(buffer + offset, &header.destNodeID, 4);
+    offset += 4;
+    memcpy(buffer + offset, &header.srcNodeID, 4);
+    offset += 4;
+    memcpy(buffer + offset, &header.packetID, 4);
+    offset += 4;
+    buffer[offset++] = header.packetType;
+    buffer[offset++] = header.flags;
+    buffer[offset++] = header.hopCount;
+    buffer[offset++] = header.reserved;
+    return offset;
+}
+
+inline size_t deserialiseBaseHeader(const uint8_t* buffer, BaseHeader &header){
+    size_t offset = 0;
+    memcpy(&header.destNodeID, buffer+offset, 4);
+    offset += 4;
+    memcpy(&header.srcNodeID, buffer+offset, 4);
+    offset += 4;
+    memcpy(&header.packetID, buffer+offset, 4);
+    offset += 4;
+    header.packetType = buffer[offset++];
+    header.flags = buffer[offset++];
+    header.hopCount = buffer[offset++];
+    header.reserved = buffer[offset++];
+    return offset;
+}
+
+inline size_t serialiseRREQHeader(const RREQHeader &header, uint8_t* buffer, size_t offset) {
+    memcpy(buffer+offset, &header.originNodeID, 4);
+    offset += 4;
+    buffer[offset++] = header.currentHops;
+    buffer[offset++] = header.rreqReserved;
+    return offset;
+}
+
+inline size_t deserialiseRREQHeader(uint8_t* buffer, RREQHeader &header, size_t offset) {
+    memcpy(&header.originNodeID, buffer+offset, 4);
+    offset += 4;
+    header.currentHops = buffer[offset++];
+    header.rreqReserved = buffer[offset++];
+    return offset;
+}
+
+
+inline size_t serialiseRREPHeader(const RREPHeader &header, uint8_t* buffer, size_t offset) {
+    memcpy(buffer+offset, &header.originNodeID, 4);
+    offset += 4;
+    memcpy(buffer + offset, &header.lifetime, 2);
+    offset += 2;
+    buffer[offset++] = header.numHops;
+    return offset;
+}
+
+inline size_t deserialiseRREPHeader(uint8_t* buffer, RREPHeader &header, size_t offset) {
+    memcpy(&header.originNodeID, buffer + offset, 4);
+    offset += 4;
+    memcpy(&header.lifetime, buffer + offset, 2);
+    header.numHops = buffer[offset++];
+    return offset;
+}
+
+inline size_t serialiseRERRHeader(const RERRHeader &header, uint8_t* buffer, size_t offset) {
+    memcpy(buffer + offset, &header.originNodeID, 4);
+    offset += 4;
+    memcpy(buffer + offset, &header.brokenNodeID, 4);
+    offset += 4;
+    memcpy(buffer + offset, &header.originalDestNodeID, 4);
+    offset += 4;
+    memcpy(buffer + offset, &header.originalPacketId, 4);
+    offset += 4;
+    return offset;
+}
+
+inline size_t deserialiseRERRHeader(const uint8_t* buffer, size_t offset, RERRHeader &header) {
+    memcpy(&header.originNodeID, buffer + offset, 4);
+    offset += 4;
+    memcpy(&header.brokenNodeID, buffer + offset, 4);
+    offset += 4;
+    memcpy(&header.originalDestNodeID, buffer + offset, 4);
+    offset += 4;
+    memcpy(&header.originalPacketId, buffer + offset, 4);
+    offset += 4;
+    return offset;
+}
+
+inline size_t serialiseACK(const ACKHeader &ack, uint8_t* buffer, size_t offset) {
+    memcpy(buffer + offset, &ack.originalPacket, 4);
+    offset += 4;
+    return offset;
+}
+
+inline size_t deserialiseACK(const uint8_t* buffer, size_t offset, ACKHeader &ack) {
+    memcpy(&ack.originalPacket, buffer + offset, 4);
+    offset += 4;
+    return offset;
+}
+
+
+#endif 
